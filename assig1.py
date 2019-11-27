7b0415cefddc870b83eadcccad71b41a23302736
Pipatpong Thadapitakporn <51953443+lKmkml@users.noreply.github.com>
2019-11-27 19:05:58 +0700

    save

---
+++ assig1.py
@@ -0,0 +1,94 @@
+def main():
+    line()
+    sentence()
+    word()
+    ABC()
+    big_letter()
+    small_letter()
+    non_letter()
+    
+def line():
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i =='\n':
+            save +=1
+        else:
+            pass
+    print("Number of line:",save)
+def sentence():
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i ==".":
+            save +=1
+        else:
+            pass
+    print("Number of Sentence:",save)
+
+def word():
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i ==" ":
+            save += 1
+        else:
+            pass
+    print("Number of Word:",save)
+
+def ABC():
+    #ABCD =['a','b','c','d','e','f','g','h','i','j','k','l','n','m','o','p','q','r','s','t','u','v','w','x','y','z']
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i.isalpha()== True:
+            save += 1
+        else:
+            pass
+    print("Number of character:",save)
+
+def big_letter():
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i.isupper()== True:
+            save += 1
+        else:
+            pass
+    print("Number of Big letter:",save)
+
+def small_letter():
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i.islower()== True:
+            save += 1
+        else:
+            pass
+    print("Number of Small letter:",save)
+
+def non_letter():
+    non= ['.',',','/','(',')',';']
+    save = 0
+    infile = open('Loop.txt','r')
+    read = infile.read()
+    infile.close()
+    for i in read:
+        if i in non:
+            save += 1
+        else:
+            pass
+    print("Number of non letter:",save)
+main()
